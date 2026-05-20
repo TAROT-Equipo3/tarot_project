63bbc6b81fae7381a1dbbc2817bffaede9db5f32
atapiamallea <a.tapia.mallea@gmail.com>
2026-05-20 12:17:28 +0200

    add files config

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function header() {
+  return (
+    <div>
+       <h2>Esto es el header</h2>
+    </div>
+  )
+}
+
+export default header
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+      <h2> Esta es la Landing </h2>
+    </div>
+  )
+}
+
+export default Home
