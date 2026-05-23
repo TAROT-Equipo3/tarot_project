76ddb62d598e33e6f215fcf07b7a553ecbbab529
nagicome03 <nayeligiselle.0206@gmail.com>
2026-05-23 20:54:44 +0200

    changes

--- src/components/NamePopup.jsx
+++ src/components/NamePopup.jsx
@@ -1,9 +1,17 @@
-import React from "react";
+import React, {useState} from "react";
 
-const NamePopup = () => {
+const NamePopup = ({onSubmitName}) => {
+  const [localName, setLocalName] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (localName.trim() !== "") {
+      onSubmitName(localName); // Envia el nombre al padre (Home.jsx)
+    }
+  };
   return (
-    <div className="fixed inset-0 flex items-center justify-center">
-      <div>
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-modal backdrop-blur-sm-">
+      <div className="p-8 max-w-sm w-full mx-4 text-center rounded-xl bg-pink-100 border-solid ">
         <h2>¿QUIÉN ME PREGUNTA?</h2>
         <form>
           <input type="text" placeholder="PON TU FIRMA CÓSMICA" required />
@@ -13,13 +21,12 @@ const NamePopup = () => {
               width="32"
               height="32"
               viewBox="0 0 32 32"
-              fill="none"
+              className="fill-goldDark"
             >
               <path
-                fill-rule="evenodd"
-                clip-rule="evenodd"
+                fillRule="evenodd"
+                clipRule="evenodd"
                 d="M8.44258 7.66401C7.06925 7.09601 5.69591 8.47335 6.26658 9.84535L8.53591 15.0853L18.5106 15.852C18.6746 15.8653 18.6746 16.1053 18.5106 16.1187L8.53325 16.8853L6.26658 22.0973C5.69458 23.4693 7.06925 24.8467 8.44258 24.2787L24.7959 17.5107C26.1692 16.9427 26.1692 14.9987 24.7959 14.4307L8.44258 7.66401Z"
-                fill="#6C5E06"
               />
             </svg>
           </button>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,28 @@
+import { useState, useEffect } from "react";
+import NamePopup from "../components/NamePopup";
+
 function Home() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [userName, setUserName] = useState("");
+
+  useEffect(() => {
+    // Activa el modal automáticamente tras 800ms
+    const timer = setTimeout(() => {
+      setIsModalOpen(true);
+    }, 800);
+
+    // Limpieza del temporizador si el componente se desmonta
+    return () => clearTimeout(timer);
+  }, []);
+
+  // Función que se ejecuta cuando el hijo envía el nombre
+  const handleSaveName = (name) => {
+    setUserName(name);
+    setIsModalOpen(false); // Cierra el modal
+  };
   return (
     <div className="w-full flex justify-center items-center text-white">
+      {isModalOpen && <NamePopup onSubmitName={handleSaveName} />}
       <h2>Esta es la Landing</h2>
     </div>
   );
