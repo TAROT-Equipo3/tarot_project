df76946170854c679c76336cebebeef8b24ca510
atapiamallea <a.tapia.mallea@gmail.com>
2026-05-25 13:23:02 +0200

    fix routerProvider

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,10 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { router } from "./config/router.jsx";
-import App from './App';
+import { RouterProvider } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
 );
